e22a8a6556889ec4202988cbcc4af520a3cd2160
jyothsnakarra <107170833+jyothsnakarra@users.noreply.github.com>
2022-07-07 20:55:01 +0530

    Create Decode_ways.cpp

---
+++ Decode_ways.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n=s.size();
+        int count[n + 1];
+        count[0] = 1;
+        count[1] = 1;
+        if(s[0] == '0')
+            return 0;
+        for(int i = 2; i <= n; i++){
+            count[i] = 0;
+            if(s[i - 1] > '0')
+                count[i] = count[i - 1];
+            if(s[i - 2] == '1' || (s[i - 2] == '2' && s[i - 1] < '7')){
+                count[i] += count[i - 2];
+            }
+        }
+        return count[n];
+    }
+};
